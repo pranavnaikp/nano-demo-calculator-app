2adf336beb2ae5396d84e7992cd8cf61e3a6303d
pranavnaikp <pranavnaik250901@gmail.com>
2023-08-20 15:12:08 +0530

    all the functions completed

--- server.js
+++ server.js
@@ -3,23 +3,30 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = '/calculator';
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const num1 = req.body.first;
+    const num2 = req.body.second;
+
+    const result = num1 + num2;
+    res.json({ "result": result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const num1 = req.body.first;
+    const num2 = req.body.second;
+
+    const result = num1 - num2;
+    res.json({ "result": result });
 });
 
 app.use(baseUrl, baseRouter);
